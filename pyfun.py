60df86acace89eee7e774851b4d9a7183d012210
Bid Ocean <lipika.karn.lk@gmail.com>
2019-01-11 16:42:05 +0530

    2 message

--- pyfun.py
+++ pyfun.py
@@ -9,11 +9,11 @@ c = 6
 
 # To take coefficient input from the users
  a = float(input('Enter a: '))
-bb = float(input('Enter b: '))
+ b = float(input('Enter b: '))
  c = float(input('Enter c: '))
 
 # calculate the discriminant
-d = (b**2) - (4*a*c)
+sum = (b**2) - (4*a*c)
 
 # find two solutions
 sol1 = (-b-cmath.sqrt(d))/(2*a)
